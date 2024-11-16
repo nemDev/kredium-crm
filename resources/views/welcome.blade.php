71901ff7ef8ad1dba66e20feabe17dbd2d9475e9
Nemanja <nem.janicijevic95@gmail.com>
2024-11-16 15:51:27 +0100

    Creating welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,5 +2,13 @@
 @section('title', 'Kredium CRM | Welcome')
     
 @section('content')
-    <h2>Welcome to Kredium CRM</h2>
+    <div class="w-full h-screen flex justify-center items-center" >
+        <h1 class="flex justify-center items-center w-2/4 m-auto text-center text-2xl">
+            Welcome to a small CRM (Custom Relationship Management) software for a
+            company that specializes in connecting their clients with banks and lenders that can
+            provide them with two types of loans (Cash and Home)
+            <br>
+            CRM is built using Laravel (version 11.9), MySQL and Tailwind.
+        </h1>
+    </div>
 @endsection
